e944b801cea8107b34ab8429d581e0f2cfb08353
Eliachar Feig <efeig15@gmail.com>
2022-05-10 17:56:58 +0300

    Update ones_diagonal_zeros_elsewhere.py

--- pythonProject/ones_diagonal_zeros_elsewhere.py
+++ pythonProject/ones_diagonal_zeros_elsewhere.py
@@ -1,7 +1,8 @@
 import numpy as np
+from numpy.typing import NDArray
 
 
-def create_identity_matrix_of_size(size: int = 2):
+def create_identity_matrix_of_size(size: int = 2) -> NDArray:
     """
     Return a square matrix of size given with ones on a diagonal and zeros elsewhere.
     :param size: The size of the matrix (rows = cols = size).
